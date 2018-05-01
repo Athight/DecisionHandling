bc2fb4ec91c89e1c2dac3a82c3547797d94fdffa
Athight <j.lallu25@gmail.com>
2018-05-01 22:29:10 +1200

    Create some probable solutions
    Create DecisionLib, can be renamed.
    Create ConsoleTest as the console PoC for this, create DecisionLibTests so we can blame each other for not testing things.

---
+++ DecisionLib/DecisionLib/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecisionLib
+{
+	public class Class1 {
+		// we should delete this class
+		// also we probably need an EventLib or something
+	}
+}
---
+++ DecisionLib/DecisionLibTests/Class1Test.cs
@@ -0,0 +1,40 @@
+﻿using DecisionLib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecisionLibTests
+{
+	[TestFixture]
+	public class Class1Test {
+
+		[Test]
+		public void Class1ExistsTest() {
+			// usually you'd set something special up using Class1 but... here goes as a terrible example...
+
+			Class1 instance = new Class1();
+
+			Assert.That(instance, Is.Not.Null);
+		}
+
+		[Test]
+		public void Class1DoesntExistTest() {
+			// these are terrible tests but hopefully they work as an example.
+
+			Class1 instance = null;
+
+			Assert.That(instance, Is.Null);
+
+			// you can assert multiple things in a test... but there shoudl really be one test per.
+			int count = 0;
+			for (int i = 1; i <= 10; i++) {
+				count += i;
+			}
+
+			Assert.That(count, Is.EqualTo((10 + 1) * 10 / 2));
+		}
+	}
+}
